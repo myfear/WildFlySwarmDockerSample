2544bae234422fce628ea1efb7c5bd4860499470
Markus Eisele <myfear@web.de>
2015-10-08 15:32:02 +0200

    Various Updates

--- src/main/java/net/eisele/test/swarm/sample/CustomerService.java
+++ src/main/java/net/eisele/test/swarm/sample/CustomerService.java
@@ -1,10 +1,10 @@
 package net.eisele.test.swarm.sample;
 
-import javax.json.Json;
-import javax.json.JsonObject;
 import javax.ws.rs.Produces;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriInfo;
 
 /**
  * REST Web Service
@@ -14,6 +14,9 @@ import javax.ws.rs.Path;
 @Path("customer")
 public class CustomerService {
 
+    @Context
+    UriInfo  context ;
+
     /**
      * Creates a new instance of CustomerService
      */
@@ -21,30 +24,15 @@ public class CustomerService {
     }
 
     /**
-     * Retrieves representation of an instance of
-     * net.eisele.test.swarm.sample.CustomerService
-     *
-     * @return an instance of java.lang.String
+     * Simple example to output request URI and hostname
+     * @return an instance of
+     * java.lang.String
      */
     @GET
     @Produces("application/json")
     public String getJson() {
-        JsonObject model = Json.createObjectBuilder()
-                .add("firstName", "Markus")
-                .add("lastName", "Eisele")
-                .add("streetAddress", "Werner-von-Siemens-Ring 14")
-                .add("city", "Grasbrunn")
-                .add("pcode", "85630")
-                .add("phoneNumbers", Json.createArrayBuilder()
-                        .add(Json.createObjectBuilder()
-                                .add("type", "mobile")
-                                .add("number", "111-111-1111"))
-                        .add(Json.createObjectBuilder()
-                                .add("type", "home")
-                                .add("number", "222-222-2222")))
-                .build();
 
-        return model.toString();
+        return "<b>WildFly Swarm Docker Application on OpenShift at " + context.getBaseUri() + " - Hostname: " +System.getenv().get("HOSTNAME") + "</b>" ;
 
     }
 
